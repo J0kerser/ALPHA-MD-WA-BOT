dac9cfeaff558416972a673fc019a80734536afd
JOKER_SER <87939150+J0kerser@users.noreply.github.com>
2022-08-24 21:57:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
+global.owner = ['919446697883']
+global.premium = ['919446697883']
 global.ownernomer = '94784506970'
-global.ownername = 'Achi Fernando'
+global.ownername = 'JOKER SER'
 global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
 global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
 global.ig = 'https://github.com/ALPHA-OFFICIAL-TEAM'
